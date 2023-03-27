d9c716620a9f8635a7f38f92d6b971141fa314d7
Yousuf MB <109029419+Alsalam-Code@users.noreply.github.com>
2023-03-27 20:08:52 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+#-------------------------------------------#
+#-- Flask => Create & Extends Html Templates
+#-------------------------------------------#
+
+from flask import Flask, render_template, url_for, request
+import sqlite3
+import mechanize
+import http
+
+
+def Send_Message():
+    print('OK APP')
+
+
+app = Flask(__name__)
+ 
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("index.html")
+
+@app.route('/result',methods=['POST', 'GET'])
+def result():
+    output = request.form.to_dict()
+    print(output)
+    name = output["name"]
+    Send_Message()
+
+    return render_template('index.html', name = name)
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
